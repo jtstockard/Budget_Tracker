6ef0ae0d65c4b2ceee4a52ba81b49b520f00ab1f
jtstockard <jtstockard92@gmail.com>
2021-07-28 21:05:35 -0500

    adding dependecies

---
+++ public/service-worker.js
@@ -0,0 +1,72 @@
+const FILES_TO_CACHE = [
+  `/`,
+  `/index.html`,
+  `/manifest.webmanifest`,
+  `/public/icons/piggy-bank-96x96.png`,
+  `/public/icons/piggy-bank-192x192.png`,
+  `/public/index.js`,
+  `/public/db.js`,
+  `/public/style.css`,
+];
+
+const CACHE_NAME = `static-cache-v2`;
+const DATA_CACHE_NAME = `data-cache-v1`;
+
+//install
+self.addEventListener(`install`, (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener(`activate`, (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            return caches.delete(key);
+          }
+          return undefined;
+        })
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener(`fetch`, (event) => {
+  if (event.request.url.includes(`/api/`)) {
+    event.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) =>
+          fetch(event.request)
+            .then((response) => {
+              if (response.status === 200) {
+                cache.put(event.request.url, response.clone());
+              }
+
+              return response;
+            })
+            .catch((err) => {
+              cache.match(event.request);
+              console.error(err);
+            })
+        )
+        .catch((err) => console.error(err))
+    );
+  } else {
+    event.respondWith(
+      caches
+        .match(event.request)
+        .then((response) => response || fetch(event.request))
+        .catch((err) => console.error(err))
+    );
+  }
+});
