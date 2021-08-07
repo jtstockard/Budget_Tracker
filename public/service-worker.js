2465bfeaae97f9c3d3a32c6ad059361c5e2a960d
jtstockard <jtstockard92@gmail.com>
2021-08-07 16:51:40 -0500

    added changes to db and sw

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,21 +1,25 @@
 const FILES_TO_CACHE = [
-  `/`,
-  `/index.html`,
-  `/manifest.webmanifest`,
-  `/public/icons/icon-192x192.png`,
-  `/public/icons/icon-512x512.png`,
-  `/public/index.js`,
-  `/public/db.js`,
-  `/public/style.css`,
+  "/",
+  "/index.html",
+  "/index.js",
+  "/db.js",
+  "/style.css",
+  "/icons/icon-144x144.png",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
+  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
 ];
 
 const CACHE_NAME = `static-cache-v2`;
 const DATA_CACHE_NAME = `data-cache-v1`;
 
 //install
-self.addEventListener(`install`, (event) => {
-  event.waitUntil(
+self.addEventListener("install", function (evt) {
+  evt.waitUntil(caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction")));
+  evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
+      console.log("Pre-Cached Files Loaded Successfully");
       return cache.addAll(FILES_TO_CACHE);
     })
   );
@@ -23,50 +27,49 @@ self.addEventListener(`install`, (event) => {
   self.skipWaiting();
 });
 
-self.addEventListener(`activate`, (event) => {
-  event.waitUntil(
-    caches.keys().then((keyList) =>
-      Promise.all(
+self.addEventListener("activate", function (evt) {
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
         keyList.map((key) => {
           if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing Cached Data", key);
             return caches.delete(key);
           }
-          return undefined;
         })
-      )
-    )
+      );
+    })
   );
 
   self.clients.claim();
 });
 
-self.addEventListener(`fetch`, (event) => {
-  if (event.request.url.includes(`/api/`)) {
-    event.respondWith(
-      caches
-        .open(DATA_CACHE_NAME)
-        .then((cache) =>
-          fetch(event.request)
-            .then((response) => {
-              if (response.status === 200) {
-                cache.put(event.request.url, response.clone());
-              }
+self.addEventListener("fetch", (evt) => {
+  if (evt.request.url.includes("/api/")) {
+    console.log("[Service Worker] Fetch(data)", evt.request.url);
 
-              return response;
-            })
-            .catch((err) => {
-              cache.match(event.request);
-              console.error(err);
-            })
-        )
-        .catch((err) => console.error(err))
+    evt.respondWith(
+      caches.open(DATA_CACHE_NAME).then((cache) => {
+        return fetch(evt.request)
+          .then((response) => {
+            if (response.status === 200) {
+              cache.put(evt.request.url, response.clone());
+            }
+            return response;
+          })
+          .catch((err) => {
+            return cache.match(evt.request);
+          });
+      })
     );
-  } else {
-    event.respondWith(
-      caches
-        .match(event.request)
-        .then((response) => response || fetch(event.request))
-        .catch((err) => console.error(err))
-    );
+    return;
   }
+
+  evt.respondWith(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(evt.request).then((response) => {
+        return response || fetch(evt.request);
+      });
+    })
+  );
 });
